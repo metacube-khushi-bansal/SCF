bd7250a6b41bc38a27736217bad2ce2cd793df89
Khushi Bansal <khushi.bansal@metacube.com>
2024-10-28 16:17:23 +0530

    updated comments

--- assignment4/src/Main/java/MathOperations.java
+++ assignment4/src/Main/java/MathOperations.java
@@ -1,39 +1,45 @@
 package Main.java;
 
 public class MathOperations {
-   
 
-   // To calculate the hcf of two numbers
-   public int calculateHcfOfTwoNumbers(int num1, int num2)
-   {
-    if(num1 <0 || num2 < 0)   // HCF of two negative numbers is not valid
-    throw new IllegalArgumentException("Input must be positive : Enter valid!");
-     
-    //HCF of 0 nad 0 is invalid
-    if(num1 ==0 && num2 ==0)
-    throw new ArithmeticException("Cant Find the HCF of 0 and 0! Enter valid");
-    
-    // Base case
-    if(num2 == 0)
-    return num1;     // if one of the number is zero, HCF is the remaining non-zero number
+   /**
+    * To calculate HCF of Two numbers num1, num2
+    * 
+    * @param num1  - First number to be taken as input
+    * @param num2- Second number to be taken as input
+    * @return int- The HCF of the numbers
+    */
+   public int calculateHcfOfTwoNumbers(int num1, int num2) {
+      if (num1 < 0 || num2 < 0)
+         throw new IllegalArgumentException("Input must be positive : Enter valid!");
 
-    else{
-        return calculateHcfOfTwoNumbers(num2, num1% num2);   // recursively call the HCF method
-    }
-    
+      if (num1 == 0 && num2 == 0)
+         throw new ArithmeticException("Cant Find the HCF of 0 and 0! Enter valid");
+
+      // Base case
+      if (num2 == 0)
+         return num1; // if one of the number is zero, HCF is the remaining non-zero number
+
+      else {
+         return calculateHcfOfTwoNumbers(num2, num1 % num2); // recursively call the HCF method
+      }
+
    }
 
+   /**
+    * To calculate LCM of Two numbers num1, num2
+    * 
+    * @param num1  - First number to be taken as input
+    * @param num2- Second number to be taken as input
+    * @return int- The LCM of the numbers
+    */
+   public int calculateLCMOfTwoNumbers(int num1, int num2) {
+      if (num1 < 0 || num2 < 0) 
+         throw new IllegalArgumentException("Input must be positive : Enter valid!");
 
-      // To calculate the LCM of two numbers
-   public int calculateLCMOfTwoNumbers(int num1, int num2)
-   {      
-          if(num1 <0 ||num2 <0)  // LCM of two negative numbers is not valid
-          throw new IllegalArgumentException("Input must be positive : Enter valid!");
-
-
-          // finding LCM of two numbers by euclidean formula
-          int ans =( num1 * num2) / calculateHcfOfTwoNumbers(num1, num2);
-          return ans;    // returning the final ans
+      // finding LCM of two numbers by euclidean formula
+      int ans = (num1 * num2) / calculateHcfOfTwoNumbers(num1, num2);
+      return ans; 
    }
 
 }
--- assignment4/src/Main/java/Search.java
+++ assignment4/src/Main/java/Search.java
@@ -2,7 +2,14 @@ package Main.java;
 
 public class Search {
 
-    // To find if a key is present or not in the array
+    /**
+     * To find if a key is present or not in the array using linear Search
+     * 
+     * @param array - Input array from which the element is to be searched
+     * @param key   - Element which is to be founf
+     * @param size  - Size of the input array
+     * @return int- Returns the index if element is found, otherwise -1
+     */
     public int linearSearch(int[] array, int key, int size) {
         if (array.length == 0) {
             throw new ArithmeticException("Array is Empty!Enter any valid size of array!");
@@ -14,39 +21,39 @@ public class Search {
         if (array[size - 1] == key)
             return size - 1; // if key found, return the index
 
-        return linearSearch(array, key, size - 1); // recursively call the linearSearch till key not found or reach end
-                                                   // of array
+        return linearSearch(array, key, size - 1);
 
     }
 
-    // to find if a key is present or not using binary search
+    /**
+     * To find if a key is present or not in the array using binary Search
+     * 
+     * @param array    - Input array from which the element is to be searched
+     * @param key      - Element which is to be founf
+     * @param leftInd  - Represents the left end of the input array
+     * @param rightInd - Represents the right end of the input array
+     * @return int- Returns the index if element is found, otherwise -1
+     */
     public int binarySearch(int[] array, int key, int leftInd, int rightInd) {
         int length = array.length;
-        if (length == 0) { // If user neter a empty array
+        if (length == 0) {
             throw new ArithmeticException("Array is Empty!Enter any valid size of array!");
         }
 
-        // check so that
         if (rightInd >= leftInd && leftInd < array.length - 1) {
-            // finding the middle index of sorted array
+
             int mid = leftInd + (rightInd - leftInd) / 2;
 
-            // if middle index array element is equal to the key
             if (array[mid] == key)
                 return mid;
 
-            // if middle element greater than the key we want to find, reduce search space
-            // to left side of the array
             else if (array[mid] > key)
                 return binarySearch(array, key, leftInd, mid - 1);
 
-            // if middle element smaller than the key we want to find, reduce search space
-            // to right side of the array
             else
                 return binarySearch(array, key, mid + 1, rightInd);
         }
-        
-        //reached the end of array, still key not found
+
         return -1; // element not found in the array
 
     }
